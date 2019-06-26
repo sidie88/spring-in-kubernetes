2427ed41025675fd19a11cb3920ea6d94294ed1f
Tarsidi <sidie88@gmail.com>
2019-06-26 18:27:18 +0700

    remove auth

--- currency-conversion-service/src/main/java/com/cxrus/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/cxrus/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -9,12 +9,11 @@ import com.cxrus.microservices.currencyconversionservice.model.CurrencyConversio
 
 //@FeignClient(name="currency-exchange-service", url="localhost:8000")
 //@FeignClient(name="currency-exchange-service")
-//@FeignClient(name="netflix-zuul-api-gateway-server")
-@FeignClient(name="currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue
 		(@PathVariable("from") String from, @PathVariable("to") String to);
 	
