01441abc94948811e857ca4576c5c58a2c0500b7
Tarsidi <sidie88@gmail.com>
2019-07-24 13:41:29 +0700

    update kafka config

--- kafka/start_kafka.sh
+++ kafka/start_kafka.sh
@@ -10,7 +10,15 @@ then
       echo "\$KAFKA_ZOOKEEPER_CONNECT_PORT is empty"
 	  KAFKA_ZOOKEEPER_CONNECT_PORT="2181"
 fi
-sed "s/\#listeners\=PLAINTEXT\:\/\/\:9092/listeners\=PLAINTEXT\:\/\/:9092/g; s/broker\.id\=0/broker.id\=$KAFKA_BROKER_ID/g; s/localhost/$KAFKA_ZOOKEEPER_CONNECT_HOST/g; s/2181/$KAFKA_ZOOKEEPER_CONNECT_PORT/g" server.properties > server_active.properties
+sed "s/broker\.id\=0/broker.id\=$KAFKA_BROKER_ID/g; s/localhost/$KAFKA_ZOOKEEPER_CONNECT_HOST/g; s/2181/$KAFKA_ZOOKEEPER_CONNECT_PORT/g; s/\#advertised\.listeners\=PLAINTEXT\:\/\/your\.host\.name\:9092/advertised\.listeners\=PLAINTEXT\:\/\/$KAFKA_ADVERTISED_HOST\:9092/g; s/\#listeners\=PLAINTEXT\:\/\/\:9092/listeners\=PLAINTEXT\:\/\/$KAFKA_ADVERTISED_HOST:9092/g" server.properties > server_active.properties
+
+if [ -z "$KAFKA_ADVERTISED_HOST" ]
+then
+      echo "\$KAFKA_ZOOKEEPER_CONNECT_PORT is empty"
+else
+	  echo -e "\n" >> server_active.properties
+	  echo "advertised.host.name=$KAFKA_ADVERTISED_HOST" >> server_active.properties
+fi
 
 cd ../
 exec bin/kafka-server-start.sh config/server_active.properties
